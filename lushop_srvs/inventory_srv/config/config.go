20a978fb54d28f420a84b6aefb4f42e5eb7f1978
LucienLSA <shenaolu0603@163.com>
2025-04-05 16:23:00 +0800

    完成库存服务，补充了其redis的初始化配置，接下来实现订单服务

--- lushop_srvs/inventory_srv/config/config.go
+++ lushop_srvs/inventory_srv/config/config.go
@@ -8,12 +8,21 @@ type MySQLConfig struct {
 	PassWord string `mapstructure:"password" json:"password"`
 }
 
+type RedisConfig struct {
+	Host     string `mapstructure:"host" json:"host"`
+	Port     string `mapstructure:"port" json:"port"`
+	Password string `mapstructure:"password" json:"password"`
+	PoolSize int    `mapstructure:"pool_size" json:"pool_size"`
+	DB       int    `mapstructure:"db" json:"db"`
+}
+
 type ServerConfig struct {
 	Name       string       `mapstructure:"name" json:"name"`
 	Host       string       `mapstructure:"host" json:"host"`
 	Tags       []string     `mapstructure:"tags" json:"tags"`
 	MySQLInfo  MySQLConfig  `mapstructure:"mysql" json:"mysql"`
 	ConsulInfo ConsulConfig `mapstructure:"consul" json:"consul"`
+	RedisInfo  RedisConfig  `mapstructure:"redis" json:"redis"`
 }
 
 type ConsulConfig struct {

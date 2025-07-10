6af6d6ae955f4388c8a5c5056b1ea49ae5049b02
LucienLSA <shenaolu0603@163.com>
2025-07-10 20:31:39 +0800

    重新补充和测试user服务，增加了双token刷新，后续考虑redis存储token实现单点登录

--- lushop_api/user_web/router/user.go
+++ lushop_api/user_web/router/user.go
@@ -13,5 +13,7 @@ func InitUserRouter(Router *gin.RouterGroup) {
 		UserRouter.GET("list", middlewares.JWTAuth(), middlewares.IsAdminAuth(), api.GetUserList)
 		UserRouter.POST("pwd_login", api.PassWordLogin)
 		UserRouter.POST("register", api.Register)
+		UserRouter.POST("refresh_token", middlewares.JWTAuth(), api.RefreshToken)
+		UserRouter.PATCH("update", middlewares.JWTAuth(), api.UpdateUser)
 	}
 }

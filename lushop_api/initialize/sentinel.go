b39b05c9d0c0856207ceb1f71d1dbeb92da30ca8
LucienLSA <shenaolu0603@163.com>
2025-07-25 18:33:15 +0800

    测试完用户服务、商品服务需测试列表查询，使用ES查询

--- lushop_api/initialize/sentinel.go
+++ lushop_api/initialize/sentinel.go
@@ -14,27 +14,34 @@ func Sentinel() {
 	if err != nil {
 		otelzap.S().Fatalf("初始化sentinel 失败:%v", err)
 	}
+	// 限流 当请求量超过阈值时，Sentinel 会立即拒绝多余请求，避免服务过载。
 	_, err = flow.LoadRules([]*flow.Rule{
 		{
 			Resource:               global.ServerConfig.SentinelInfo.App.Name,
-			TokenCalculateStrategy: flow.Direct,
-			ControlBehavior:        flow.Reject,
-			Threshold:              6,
-			StatIntervalInMs:       global.ServerConfig.SentinelInfo.Stat.GlobalStatisticIntervalMsTotal,
+			TokenCalculateStrategy: flow.Direct, // 直接统计请求量（非冷启动模式）
+			ControlBehavior:        flow.Reject, // Reject 表示超阈值时直接拒绝请求（快速失败）
+			Threshold:              6,           // 每秒允许的最大请求数为 6（超过则触发限流）
+			StatIntervalInMs: global.ServerConfig.SentinelInfo.
+				Stat.GlobalStatisticIntervalMsTotal, // 统计时间窗口（从配置读取，单位毫秒）。
 		},
 	})
+	if err != nil {
+		otelzap.S().Fatalf("加载限流规则失败:%v", err)
+	}
+	// 熔断降级
 	_, err = circuitbreaker.LoadRules([]*circuitbreaker.Rule{
 		// Statistic time span=5s, recoveryTimeout=3s, maxErrorCount=50
 		{
 			Resource:         global.ServerConfig.SentinelInfo.App.Name,
-			Strategy:         circuitbreaker.ErrorRatio,
-			RetryTimeoutMs:   3000, // 3s之后尝试恢复
-			MinRequestAmount: 10,   // 静默数
-			StatIntervalMs:   global.ServerConfig.SentinelInfo.Stat.GlobalStatisticIntervalMsTotal,
-			Threshold:        0.4,
+			Strategy:         circuitbreaker.ErrorRatio, // ErrorRatio 表示基于 错误比例 触发熔断。
+			RetryTimeoutMs:   3000,                      // 熔断后 3 秒 尝试恢复（半开状态探测）。
+			MinRequestAmount: 10,                        // 静默数
+			StatIntervalMs: global.ServerConfig.SentinelInfo.
+				Stat.GlobalStatisticIntervalMsTotal, // 统计时间窗口
+			Threshold: 0.4, // 错误比例阈值为 0.4（即 40% 请求失败时熔断）。
 		},
 	})
 	if err != nil {
-		otelzap.S().Fatalf("加载规则失败:%v", err)
+		otelzap.S().Fatalf("加载熔断降级规则失败:%v", err)
 	}
 }
--- lushop_srvs/goods_srv/handler/goods.go
+++ lushop_srvs/goods_srv/handler/goods.go
@@ -2,11 +2,13 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"goodssrv/global"
 	"goodssrv/model"
 	proto "goodssrv/proto"
 
+	"github.com/olivere/elastic/v7"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -89,7 +91,7 @@ func (s *GoodsServer) GoodsList(ctx context.Context, req *proto.GoodsFilterReque
 		//q = q.Filter(elastic.NewRangeQuery("shop_price").Lte(req.PriceMax))
 	}
 	if req.Brand > 0 {
-		localDB = localDB.Where("brands_id=?", req.Brand)
+		localDB = localDB.Where("brand_id=?", req.Brand)
 		//q = q.Filter(elastic.NewTermQuery("brands_id", req.Brand))
 	}
 	//通过category去查询商品
@@ -157,7 +159,7 @@ func (s *GoodsServer) GoodsList(ctx context.Context, req *proto.GoodsFilterReque
 	//}
 	//查询id在某个数组中的值
 
-	re := localDB.Preload("Category").Preload("Brands").Find(&goods)
+	re := localDB.Preload("Category").Preload("Brand").Find(&goods)
 	if re.Error != nil {
 		return nil, re.Error
 	}
@@ -168,127 +170,127 @@ func (s *GoodsServer) GoodsList(ctx context.Context, req *proto.GoodsFilterReque
 	return goodsListResponse, nil
 }
 
-// // 商品接口
-// // 关键词搜索、查询新品、查询热门商品、通过价格区间筛选、通过商品分类筛选
-// func (s *GoodsServer) GoodsListES(ctx context.Context, req *proto.GoodsFilterRequest) (*proto.GoodsListResponse, error) {
-// 	// 各种条件查询
-// 	goodsListRsp := &proto.GoodsListResponse{}
-// 	// match bool 复合查询
-// 	q := elastic.NewBoolQuery()
-// 	// localDB := global.DB.Model(&model.Goods{}).Session(&gorm.Session{SkipDefaultTransaction: true})
-// 	// localDB := global.DB.Model(&model.Goods{}).Session(&gorm.Session{})
-// 	localDB := global.DB.Model(model.Goods{})
-// 	if req.KeyWords != "" {
-// 		// localDB = localDB.Where("name LIKE ?", "%"+req.KeyWords+"%")
-// 		q = q.Must(elastic.NewMultiMatchQuery(req.KeyWords, "name", "goods_brief"))
-// 	}
-// 	if req.IsHot {
-// 		// localDB.Where("is_hot=true")
-// 		// localDB = localDB.Where(model.Goods{IsHot: true})
-// 		q = q.Filter(elastic.NewTermQuery("is_hot", req.IsHot))
-// 	}
-// 	if req.IsNew {
-// 		// localDB = localDB.Where("is_new=true")
-// 		q = q.Filter(elastic.NewTermQuery("is_hot", req.IsHot))
-// 	}
-// 	if req.PriceMin > 0 {
-// 		// localDB = localDB.Where("shop_price >= ?", req.PriceMin)
-// 		q = q.Filter(elastic.NewRangeQuery("shop_price").Gte(req.PriceMin))
-// 	}
-// 	if req.PriceMax > 0 {
-// 		// localDB = localDB.Where("shop_price <= ?", req.PriceMax)
-// 		q = q.Filter(elastic.NewRangeQuery("shop_price").Lte(req.PriceMax))
-// 	}
-// 	if req.Brand > 0 {
-// 		// localDB = localDB.Where("brand_id=?", req.Brand)
-// 		q = q.Filter(elastic.NewTermQuery("brand_id", req.Brand))
-// 	}
-// 	fmt.Println(req.TopCategory)
-// 	// var sqlQuery string
-// 	// 通过category查询商品
-// 	categoryIds := make([]interface{}, 0)
-// 	if req.TopCategory > 0 {
-// 		var category model.Category
-// 		result := global.DB.Where("id=?", req.TopCategory).First(&category)
-// 		if result.RowsAffected == 0 {
-// 			return nil, status.Errorf(codes.NotFound, "商品分类不存在")
-// 		}
-// 		subQuery := global.DB.Model(&model.Category{}).Select("id")
-// 		if category.Level == 1 {
-// 			// sqlQuery = fmt.Sprintf("SELECT id FROM category WHERE parent_category_id IN (SELECT id FROM category WHERE parent_category_id=%d)",
-// 			// 	req.TopCategory)
-// 			// 查询二级分类ID（一级分类的子分类）
-// 			subQuery = subQuery.Where("parent_category_id IN (?)",
-// 				global.DB.Model(&model.Category{}).
-// 					Select("id").
-// 					Where("parent_category_id = ?", req.TopCategory))
-// 		} else if category.Level == 2 {
-// 			// sqlQuery = fmt.Sprintf("SELECT id FROM category WHERE parent_category_id =%d",
-// 			// 	req.TopCategory)
-// 			// 查询三级分类ID（二级分类的子分类）
-// 			subQuery = subQuery.Where("parent_category_id = ?", req.TopCategory)
-// 			// localDB = localDB.Joins(
-// 			// 	"JOIN category c3 ON goods.category_id = c3.id "+
-// 			// 		"JOIN category c2 ON c3.parent_category_id = c2.id "+
-// 			// 		"WHERE c2.id = ?", req.TopCategory)
-// 		} else if category.Level == 3 {
-// 			// sqlQuery = fmt.Sprintf("SELECT id FROM category WHERE id =%d", req.TopCategory)
-// 			subQuery = subQuery.Where("id = ?", req.TopCategory)
-// 		}
-// 		// localDB = localDB.Where(fmt.Sprintf("category_id IN (%s)", sqlQuery))
-// 		// localDB = localDB.Where("category_id IN (?)", subQuery)
-// 		type Result struct {
-// 			ID int32
-// 		}
-// 		var results []Result
-// 		if err := subQuery.Scan(&results).Error; err != nil {
-// 			return nil, status.Errorf(codes.Internal, "获取商品总数失败: %v", err)
-// 		}
-// 		for _, re := range results {
-// 			categoryIds = append(categoryIds, re.ID)
-// 		}
-// 		//生成terms查询
-// 		q = q.Filter(elastic.NewTermsQuery("category_id", categoryIds...))
-// 	}
+// 商品接口
+// 关键词搜索、查询新品、查询热门商品、通过价格区间筛选、通过商品分类筛选
+func (s *GoodsServer) GoodsListES(ctx context.Context, req *proto.GoodsFilterRequest) (*proto.GoodsListResponse, error) {
+	// 各种条件查询
+	goodsListRsp := &proto.GoodsListResponse{}
+	// match bool 复合查询
+	q := elastic.NewBoolQuery()
+	// localDB := global.DB.Model(&model.Goods{}).Session(&gorm.Session{SkipDefaultTransaction: true})
+	// localDB := global.DB.Model(&model.Goods{}).Session(&gorm.Session{})
+	localDB := global.DB.Model(model.Goods{})
+	if req.KeyWords != "" {
+		// localDB = localDB.Where("name LIKE ?", "%"+req.KeyWords+"%")
+		q = q.Must(elastic.NewMultiMatchQuery(req.KeyWords, "name", "goods_brief"))
+	}
+	if req.IsHot {
+		// localDB.Where("is_hot=true")
+		// localDB = localDB.Where(model.Goods{IsHot: true})
+		q = q.Filter(elastic.NewTermQuery("is_hot", req.IsHot))
+	}
+	if req.IsNew {
+		// localDB = localDB.Where("is_new=true")
+		q = q.Filter(elastic.NewTermQuery("is_hot", req.IsHot))
+	}
+	if req.PriceMin > 0 {
+		// localDB = localDB.Where("shop_price >= ?", req.PriceMin)
+		q = q.Filter(elastic.NewRangeQuery("shop_price").Gte(req.PriceMin))
+	}
+	if req.PriceMax > 0 {
+		// localDB = localDB.Where("shop_price <= ?", req.PriceMax)
+		q = q.Filter(elastic.NewRangeQuery("shop_price").Lte(req.PriceMax))
+	}
+	if req.Brand > 0 {
+		// localDB = localDB.Where("brand_id=?", req.Brand)
+		q = q.Filter(elastic.NewTermQuery("brand_id", req.Brand))
+	}
+	fmt.Println(req.TopCategory)
+	// var sqlQuery string
+	// 通过category查询商品
+	categoryIds := make([]interface{}, 0)
+	if req.TopCategory > 0 {
+		var category model.Category
+		result := global.DB.Where("id=?", req.TopCategory).First(&category)
+		if result.RowsAffected == 0 {
+			return nil, status.Errorf(codes.NotFound, "商品分类不存在")
+		}
+		subQuery := global.DB.Model(&model.Category{}).Select("id")
+		if category.Level == 1 {
+			// sqlQuery = fmt.Sprintf("SELECT id FROM category WHERE parent_category_id IN (SELECT id FROM category WHERE parent_category_id=%d)",
+			// 	req.TopCategory)
+			// 查询二级分类ID（一级分类的子分类）
+			subQuery = subQuery.Where("parent_category_id IN (?)",
+				global.DB.Model(&model.Category{}).
+					Select("id").
+					Where("parent_category_id = ?", req.TopCategory))
+		} else if category.Level == 2 {
+			// sqlQuery = fmt.Sprintf("SELECT id FROM category WHERE parent_category_id =%d",
+			// 	req.TopCategory)
+			// 查询三级分类ID（二级分类的子分类）
+			subQuery = subQuery.Where("parent_category_id = ?", req.TopCategory)
+			// localDB = localDB.Joins(
+			// 	"JOIN category c3 ON goods.category_id = c3.id "+
+			// 		"JOIN category c2 ON c3.parent_category_id = c2.id "+
+			// 		"WHERE c2.id = ?", req.TopCategory)
+		} else if category.Level == 3 {
+			// sqlQuery = fmt.Sprintf("SELECT id FROM category WHERE id =%d", req.TopCategory)
+			subQuery = subQuery.Where("id = ?", req.TopCategory)
+		}
+		// localDB = localDB.Where(fmt.Sprintf("category_id IN (%s)", sqlQuery))
+		// localDB = localDB.Where("category_id IN (?)", subQuery)
+		type Result struct {
+			ID int32
+		}
+		var results []Result
+		if err := subQuery.Scan(&results).Error; err != nil {
+			return nil, status.Errorf(codes.Internal, "获取商品总数失败: %v", err)
+		}
+		for _, re := range results {
+			categoryIds = append(categoryIds, re.ID)
+		}
+		//生成terms查询
+		q = q.Filter(elastic.NewTermsQuery("category_id", categoryIds...))
+	}
 
-// 	// 分页
-// 	if req.Pages == 0 {
-// 		req.Pages = 1
-// 	}
+	// 分页
+	if req.Pages == 0 {
+		req.Pages = 1
+	}
 
-// 	switch {
-// 	case req.PagePerNums > 10:
-// 		req.PagePerNums = 10
-// 	case req.PagePerNums <= 0:
-// 		req.PagePerNums = 10
-// 	}
-// 	result, err := global.EsClient.Search().Index(model.EsGoods{}.GetIndexName()).
-// 		Query(q).From(int(req.Pages)).Size(int(req.PagePerNums)).
-// 		Do(context.Background())
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	switch {
+	case req.PagePerNums > 10:
+		req.PagePerNums = 10
+	case req.PagePerNums <= 0:
+		req.PagePerNums = 10
+	}
+	result, err := global.EsClient.Search().Index(model.EsGoods{}.GetIndexName()).
+		Query(q).From(int(req.Pages)).Size(int(req.PagePerNums)).
+		Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
 
-// 	goodsIds := make([]int32, 0)
-// 	goodsListRsp.Total = int32(result.Hits.TotalHits.Value)
-// 	for _, value := range result.Hits.Hits {
-// 		goods := model.EsGoods{}
-// 		_ = json.Unmarshal(value.Source, &goods)
-// 		goodsIds = append(goodsIds, goods.ID)
-// 	}
+	goodsIds := make([]int32, 0)
+	goodsListRsp.Total = int32(result.Hits.TotalHits.Value)
+	for _, value := range result.Hits.Hits {
+		goods := model.EsGoods{}
+		_ = json.Unmarshal(value.Source, &goods)
+		goodsIds = append(goodsIds, goods.ID)
+	}
 
-// 	// 查询id在某个数组中的值
-// 	var goods []model.Goods
-// 	res := localDB.Preload("Category").Preload("Brand").Find(&goods, goodsIds)
-// 	if res.Error != nil {
-// 		return nil, res.Error
-// 	}
-// 	for _, good := range goods {
-// 		goodsInfoRsp := ModelToResponse(good)
-// 		goodsListRsp.Data = append(goodsListRsp.Data, &goodsInfoRsp)
-// 	}
-// 	return goodsListRsp, nil
-// }
+	// 查询id在某个数组中的值
+	var goods []model.Goods
+	res := localDB.Preload("Category").Preload("Brand").Find(&goods, goodsIds)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	for _, good := range goods {
+		goodsInfoRsp := ModelToResponse(good)
+		goodsListRsp.Data = append(goodsListRsp.Data, &goodsInfoRsp)
+	}
+	return goodsListRsp, nil
+}
 
 // 现在用户提交订单有多个商品，批量查询商品的信息
 func (s *GoodsServer) BatchGetGoods(ctx context.Context, req *proto.BatchGoodsIdInfo) (*proto.GoodsListResponse, error) {
@@ -321,7 +323,7 @@ func (s *GoodsServer) GetGoodsDetail1(ctx context.Context, req *proto.GoodInfoRe
 func (s *GoodsServer) GetGoodsDetail(ctx context.Context, req *proto.GoodInfoRequest) (*proto.GoodsInfoResponse, error) {
 	var goods model.Goods
 
-	if result := global.DB.Preload("Brands").Preload("Category").Where(req.Id).First(&goods); result.RowsAffected == 0 {
+	if result := global.DB.Preload("Brand").Preload("Category").Where(req.Id).First(&goods); result.RowsAffected == 0 {
 		return nil, status.Errorf(codes.InvalidArgument, "参数无效")
 	}
 	goodsInfoResponse := ModelToResponse(goods)

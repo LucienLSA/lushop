11e101178140898c6d2134f74edcb63c912c97c3
LucienLSA <shenaolu0603@163.com>
2025-07-29 16:06:29 +0800

    删除lushp_api_demo

--- lushop_api_demo/user_web/forms/sms.go
+++
@@ -1,7 +0,0 @@
-package forms
-
-type SendSmsForm struct {
-	Mobile string `form:"mobile" json:"mobile" binding:"required,mobile"` // 手机号码格式规范，自定义validator
-	Type   int    `form:"type" json:"type" binding:"required,oneof=1 2"`  // 手机号码格式规范，自定义validator
-	// 注册发送短信验证码，动态验证码登录发送验证码
-}
